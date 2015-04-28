4ee724b792442a6c7d8ab6955b1a090a982b5f06
Romuald Texier-Marcadé <romuald.texier-marcade@net-ng.com>
2015-04-28 18:09:16 +0200

    Restrict paste version to 1.7.5

--- setup.py
+++ setup.py
@@ -38,7 +38,8 @@ setup(
     install_requires=(
         'PEAK-Rules==0.5a1.dev-r2713',
         'nagare[database,i18n]', 'docutils', 'Pillow', 'pycrypto', 'Babel',
-        'requests', 'oauth2', 'SQLAlchemy==0.9.8', 'dateutils', 'xlwt'
+        'requests', 'oauth2', 'SQLAlchemy==0.9.8', 'dateutils', 'xlwt',
+        'Paste==1.7.5'
     ),
     extras_require={'test': ('nose',),
                     'debug': ('WebError',),

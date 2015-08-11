ed57f1c138ed5ca9e24020b6a44f2b38e368e3bb
Romuald Texier-Marcadé <romuald.texier-marcade@net-ng.com>
2015-08-11 13:29:49 +0200

    Removed dead code

--- kansha/authentication/login.py
+++ kansha/authentication/login.py
@@ -45,38 +45,6 @@ def render_Header(self, h, comp, *args):
     return h.root
 
 
-class Credits(object):
-
-    def __init__(self, app_title, custom_css):
-        self.app_title = app_title
-        self.header = component.Component(Header(self.app_title, custom_css))
-
-
-@presentation.render_for(Credits)
-def render_Credits(self, h, comp, model):
-    h << self.header
-    with h.div(class_='credits-content'):
-        with h.ol:
-            with h.li:
-                h << h.span(u'One')
-                h << h.p(u'''Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam pharetra dolor vitae neque ullamcorper accumsan id id risus. Aliquam tempus libero tempor, varius tortor vel, congue diam. Aenean enim enim, bibendum in libero ac, hendrerit ultricies nunc. Sed placerat ante nec ligula viverra rhoncus. In vitae quam condimentum, dapibus augue et, blandit nulla. Phasellus fringilla placerat nisl, non porta tortor semper vitae. Proin lectus lorem, pretium id vulputate a, scelerisque non nisl. Pellentesque lorem ante, blandit eget fermentum id, aliquam sed justo. Quisque at massa a neque imperdiet ornare eu eu diam. Donec sed molestie velit. Donec adipiscing ullamcorper aliquam. Nulla euismod erat ut egestas malesuada. Aenean quis justo pellentesque, pulvinar justo id, fermentum libero. Donec sit amet est ut lorem ullamcorper condimentum.''')
-            with h.li:
-                h << h.span(u'Two')
-                h << h.p(u'''Pellentesque urna tortor, dignissim vel ligula quis, varius volutpat turpis. Suspendisse varius non massa vel imperdiet. Cras eget justo dictum, eleifend urna ut, facilisis nisi. Donec at dolor eu arcu sodales rhoncus eget vitae augue. Mauris quam dui, varius a enim in, hendrerit luctus leo. Morbi purus dolor, ullamcorper vel ante egestas, luctus condimentum magna. Praesent lobortis lacus ut auctor pulvinar.''')
-            with h.li:
-                h << h.span(u'Three')
-                h << h.p(u'''Aenean in ultricies justo. Mauris pulvinar, leo at auctor iaculis, tortor eros iaculis ante, eget tempus nisi nisl sed enim. Mauris ullamcorper ipsum arcu, sed commodo nisl vestibulum posuere. Proin semper neque quis suscipit condimentum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed est felis, viverra in nulla eget, posuere dignissim lectus. Etiam neque sem, pellentesque non dignissim vitae, suscipit pretium leo. Sed pharetra non dolor ut tempus. Sed vehicula orci quis laoreet cursus.''')
-            with h.li:
-                h << h.span(u'Four')
-                h << h.p(u'''Donec nec arcu tortor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec nunc eros, convallis ut lectus nec, gravida ultrices dui. Vestibulum malesuada ornare nisi. Nunc convallis velit erat, sit amet tempor tellus tempor eget. Integer volutpat, neque nec convallis tempus, libero metus cursus nulla, et aliquet nulla mi eu dolor. Morbi ante tortor, fringilla quis vestibulum nec, luctus eu sem.''')
-            with h.li:
-                h << h.span(u'Five')
-                h << h.p(u'''Pellentesque velit arcu, congue et congue bibendum, varius in risus. Suspendisse egestas tincidunt elit nec laoreet. Morbi molestie risus sollicitudin tristique luctus. Morbi sed massa quis mauris convallis lacinia eu ut massa. Integer at nulla ac metus sollicitudin bibendum. Aliquam erat volutpat. Duis eget erat elementum, varius odio et, rhoncus dui. Vestibulum porta eget magna sit amet auctor. Integer ultricies elit ut vestibulum consequat. Fusce quis quam malesuada, interdum leo eu, posuere nibh. Phasellus pulvinar augue nec urna dapibus porttitor. Etiam metus diam, porttitor eu justo bibendum, imperdiet suscipit nibh. ''')
-        with h.p:
-            h << h.a(_(u'Back')).action(comp.answer)
-    return h.root
-
-
 class Login(object):
 
     def __init__(self, app_title, custom_css, mail_sender, auth_cfg, assets_manager):

f115a1970d3a9b1c20e7a743a191fab4873e9d7d
Bertrand Croq <bertrand.croq@net-ng.com>
2015-10-22 16:00:06 +0200

    last_login mustn't be empty for already existing users

--- kansha/alembic/versions/4fb34b202c5f_last_login_stamp.py
+++ kansha/alembic/versions/4fb34b202c5f_last_login_stamp.py
@@ -1,3 +1,12 @@
+#--
+# Copyright (c) 2012-2014 Net-ng.
+# All rights reserved.
+#
+# This software is licensed under the BSD License, as described in
+# the file LICENSE.txt, which you should have received as part of
+# this distribution.
+#--
+
 """last_login stamp
 
 Revision ID: 4fb34b202c5f
@@ -6,16 +15,31 @@ Create Date: 2015-05-18 19:00:56.564336
 
 """
 
+import datetime
+
+import sqlalchemy as sa
+from alembic import op
+
 # revision identifiers, used by Alembic.
 revision = '4fb34b202c5f'
 down_revision = '6cb3c668d65'
 
-from alembic import op
-import sqlalchemy as sa
-
 
 def upgrade():
-    op.add_column('user', sa.Column('last_login', sa.DateTime))
+    last_login = sa.Column('last_login', sa.DateTime)
+    op.add_column('user', last_login)
+
+    # last_login mustn't be empty for already existing users
+    user = sa.table(
+        'user',
+        last_login,
+        sa.Column('email_to_confirm', sa.Unicode(255))
+    )
+    op.execute(
+        user.update().where(
+            user.c.email_to_confirm == None  # noqa
+        ).values({'last_login': datetime.datetime.now()})
+    )
 
 
 def downgrade():

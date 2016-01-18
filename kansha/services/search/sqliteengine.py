7f146e243240dbed40e76244a95626d06841649f
Romuald Texier-Marcadé <romuald.texier-marcade@net-ng.com>
2016-01-18 18:24:47 +0100

    Schema Mappers for index creation. SQLite implementation.

--- kansha/services/search/sqliteengine.py
+++ kansha/services/search/sqliteengine.py
@@ -70,12 +70,60 @@ class SQLiteFTSQueryMapper(object):
         return (query, params)
 
 
+class SQLiteFTSSchemaMapper(object):
+
+    def __init__(self, db_cursor):
+        self.db_cursor = db_cursor
+        self.mappings = {}
+
+    # Schema API
+
+    def define(self, schema_name):
+        self.mappings[schema_name] = {
+            'notindexed': ['notindexed=id'],
+            'fields': []
+        }
+
+    def define_field(self, schema_name, field_type, name, indexed, stored):
+        # SQLite fts tables are not typed, ignoring field_type
+        # SQLite fts always stores, ignoring stored.
+        mapping = self.mappings[schema_name]
+        if not indexed:
+            mapping['notindexed'].append("notindexed=" + name)
+        mapping['fields'].append(name)
+
+    # specific API
+
+    def create(self):
+        for schema_name, mapping in self.mappings.iteritems():
+            self.db_cursor.execute('drop table if exists %s' % schema_name)
+            slversion = sqlite3.sqlite_version_info[:3]
+            if slversion > (3, 8, 0):
+                self.db_cursor.execute(
+                    '''CREATE VIRTUAL TABLE %s USING fts4(id, %s, prefix="3,5,7", %s);''' %
+                    (schema_name,
+                     ','.join(mapping['fields']),
+                     ','.join(mapping['notindexed']))
+                )
+            elif slversion > (3, 7, 7):
+                print "Warning: older version of sqlite3 detected, please upgrade to sqlite 3.8.0 or newer."
+                self.db_cursor.execute(
+                    '''CREATE VIRTUAL TABLE %s USING fts4(id, %s, prefix="3,5,7");''' %
+                    (schema_name,
+                     ','.join(mapping['fields']))
+                )
+            else:
+                raise ImportError('Your version of sqlite3 is too old, please upgrade to sqlite 3.8.0 or newer.')
+
+
 class IndexCursor(object):
 
     def __init__(self,  db_cursor):
         self.db_cursor = db_cursor
         self.query = ''
         self.params = []
+
+    # Document API
 
     def insert(self, schema_name, docid, **fields):
         # neither schema nor fields keys are user inputs, so this is safe
@@ -97,6 +145,8 @@ class IndexCursor(object):
         self.query = 'update %s set %s where id=?' % (
             schema_name, ', '.join(qset))
         self.params.append(docid)
+
+    # Specific API
 
     def execute(self):
         self.db_cursor.execute(self.query, self.params)
@@ -211,29 +261,8 @@ class SQLiteFTSEngine(object):
         `schemas` is a list of Document classes or Schema instances.
         '''
         c = self._get_cursor()
+        mapper = SQLiteFTSSchemaMapper(c)
         for schema in schemas:
-            # no type declaration on FTS tables, so we ignore
-            # schema.fields.values
-            c.execute('drop table if exists %s' % schema.type_name)
-            notindexed = ['notindexed=id']
-            for fname, field in schema.fields.iteritems():
-                if not field.indexed:
-                    notindexed.append("notindexed=" + fname)
-            slversion = sqlite3.sqlite_version_info[:3]
-            if slversion > (3, 8, 0):
-                c.execute(
-                    '''CREATE VIRTUAL TABLE %s USING fts4(id, %s, prefix="3,5,7", %s);''' %
-                    (schema.type_name,
-                     ','.join(schema.fields.keys()),
-                     ','.join(notindexed))
-                )
-            elif slversion > (3, 7, 7):
-                print "Warning: older version of sqlite3 detected, please upgrade to sqlite 3.8.0 or newer."
-                c.execute(
-                    '''CREATE VIRTUAL TABLE %s USING fts4(id, %s, prefix="3,5,7");''' %
-                    (schema.type_name,
-                     ','.join(schema.fields.keys()))
-                )
-            else:
-                raise ImportError('Your version of sqlite3 is too old, please upgrade to sqlite 3.8.0 or newer.')
+            schema.map(mapper)
+        mapper.create()
         self.commit()

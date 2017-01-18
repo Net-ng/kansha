134bb269507dcff88662bf0a45bb02d570730af2
Romuald Texier-Marcadé <romuald.texier-marcade@net-ng.com>
2017-01-18 19:09:14 +0100

    Faster history log display. Changed the semantics of the filters.

--- kansha/services/actionlog/view.py
+++ kansha/services/actionlog/view.py
@@ -19,23 +19,25 @@ def render_ActionLog(self, h, comp, *args):
     return comp.render(h.AsyncRenderer(), 'history')
 
 
+# FIXME: demeter, foreign data direct access
 @presentation.render_for(ActionLog, 'history')
 def render_ActionLog_history(self, h, *_args):
     h << h.h2(_('Action log'))
-    board = self.board
+    log = self.get_history()
+    users = set(event.user for event in log)
+    cards = set(event.card for event in log)
     with h.div(id='action-log'):
         with h.form:
             with h.select(onchange=ajax.Update(action=self.user_id)):
                 h << h.option(_('all users'), value='')
-                for member in board.all_members:
-                    h << h.option(member().fullname, value=member().username).selected(
+                for member in sorted(users, key=lambda u: u.fullname):
+                    h << h.option(member.fullname, value=member.username).selected(
                         self.user_id())
-            with h.select(onchange=ajax.Update(action=lambda x: self.card_id(x))):
+            with h.select(onchange=ajax.Update(action=lambda x: self.card_id(int(x)))):
                 h << h.option(_('all cards'), value=0)
-                for col in board.columns:
-                    for card in col().cards:
-                        h << h.option(card().get_title(), value=card().id).selected(
-                            self.card_id())
+                for card in sorted(cards, key=lambda c: c.title):
+                    h << h.option(card.title, value=card.id).selected(
+                        self.card_id())
         with h.div(class_='history'):
             with h.table(class_='table table-striped table-hover'):
                 with h.body:
